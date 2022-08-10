c5df7e53ea12dd84f44ff35fd8a04fbac949d623
Gabriel Gordbegli <ggordbegli27@massmutual.com>
2022-08-10 10:23:25 -0400

    updated colors

--- Components/PageHeader.js
+++ Components/PageHeader.js
@@ -34,7 +34,7 @@ import { contractAddress, abi } from '../constants'
     return (
       <header className="bg-transparent">
         <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
-          <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
+          <div className="w-full py-6 flex items-center justify-between border-b border-blue-700 lg:border-none">
             <div className="flex items-center">
               <a href="#">
                 <span className="sr-only">Workflow</span>
@@ -58,7 +58,7 @@ import { contractAddress, abi } from '../constants'
               </input>
               <button
                 href="#"
-                className="inline-block bg-indigo-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75"
+                className="inline-block bg-blue-700 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75"
                 onClick={
                   async (event) => {
                     event.preventDefault()
@@ -71,7 +71,7 @@ import { contractAddress, abi } from '../constants'
               </button>
               <button
                 href="#"
-                className="inline-block bg-indigo-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75"
+                className="inline-block bg-blue-700 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75"
                 onClick={
                   async () => {
                     await enableWeb3()
--- Components/buttons/Yes.js
+++ Components/buttons/Yes.js
@@ -26,7 +26,7 @@ function Yes({id, disabled, userContext, setTransaxHash, setProcessTransax}) {
     return (
         <button
             type="button"
-            className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-blue-700 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-700"
             disabled={disabled}
             onClick={
                 async () => {
